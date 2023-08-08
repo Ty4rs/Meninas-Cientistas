b8c95372ce9a11b01cc8352714c24b9df4743a21
Ty4rs <95915769+Ty4rs@users.noreply.github.com>
2023-08-08 14:40:43 -0300

    Meninas Cientistas 9

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Captura o elemento que será movido
 const elementoMovel = document.getElementsByClassName('banner')[0]
 const cards = document.getElementsByClassName("home-cards")[0]
+const titulopr = document.getElementsByClassName("titulopr")[0]
 
-
-let var1, var2
+let var1, var2, var3
 
 // Função para mover o elemento
 function moverElemento() {
@@ -12,13 +12,22 @@ function moverElemento() {
 
     // Define a nova posição vertical do elemento
   //var novaPosicao = scrollTop ; // Pode ajustar o valor conforme desejado
-     var1 =- (window.scrollY/ (window.scrollY + 120) * 14)
-     var2 =- (window.scrollY/ (window.scrollY + 140) * 30)
+     var1 =- (window.scrollY/ (window.scrollY + 220) * 14)
+     var2 =- (window.scrollY/ (window.scrollY + 240) * 30)
+     if(var3 > -200){
+      var3 =- ((window.scrollY/ (window.scrollY - 500) ) )
+      window.alert("Oi")
+     }
+     console.log("var3" + var3)
+     
+
     // Aplica a nova posição ao elemento
     document.getElementsByClassName('banner')[0].style.top =    (var1 - 1.2) + "rem"
     document.getElementsByClassName("home-cards")[0].style.top =    var2 + "rem"
+    document.getElementsByClassName("titulopr")[0].style.top =    var3 + "rem"
     console.log(document.getElementsByClassName('banner')[0].style.top)
     console.log(var1)
+
 
 
 }
@@ -48,17 +57,18 @@ for (let index = 0; index < document.getElementsByClassName('box').length; index
         
        
   // Quando o elemento entra na tela
-        box.style.opacity = 1;
+        //box.style.opacity = 1;
         
        
-        box.style.transform = 'translateY(20px)';
+        box.style.marginTop = "-4rem"
 
       } else {
         
        
   // Quando o elemento sai da tela
-        box.style.opacity = 0;
-        box.style.transform = 'translateY(-20px)';
+        //box.style.opacity = 0;
+        //box.style.transform = 'translateY(+20px)';
+        box.style.marginTop = "1rem"
 
       }
     });
